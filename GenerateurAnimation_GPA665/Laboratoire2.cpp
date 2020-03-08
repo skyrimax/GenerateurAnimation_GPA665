49f858a6b9651342b1acfd121653afa96f629708
Maxime Royal <MaxRoyal123@hotmail.com>
2020-03-08 01:10:36 -0500

    Commented Laboratoire2 Kept already commented sections in french

--- GenerateurAnimation_GPA665/Laboratoire2.cpp
+++ GenerateurAnimation_GPA665/Laboratoire2.cpp
@@ -15,18 +15,23 @@ void Execute(ExecuteMode ExecMode, const char *sScriptFile)
 {
 	static Animation Anim;	//Structure contenant l'information générale sur votre structure de données
 
+	// Chose action depending on ExecMode
 	switch (ExecMode)
 	{
 	case emOpenScript:
+		// Load the animation from sScriptFile
 		OpenScript(&Anim, sScriptFile);
 		break;
 	case emFreeScript:
+		// Unload the animation
 		FreeScript(&Anim);
 		break;
 	case emPlayForward:
+		// Plays the animation forward
 		PlayForwardScript(&Anim);
 		break;
 	case emPlayBackward:
+		// Play the animation backward
 		PlayBackwardScript(&Anim);
 		break;
 	default:
@@ -41,16 +46,22 @@ void Execute(ExecuteMode ExecMode, const char *sScriptFile)
 //------------------------------------------------------------
 int OpenScript(Animation *Anim, const char *sScriptFile)
 {
+	// Unload animation if is isn't empty
 	if (Anim->unload()) {
+		// Load animation from sScriptFile
 		if (!Anim->load(sScriptFile)) {
+			// If there is an error while the animation is being loaded,
+			// Will unload animation and return that an error occurred
 			Anim->unload();
 			return false;
 		}
 	}
+	// If unloading fails, return that an error occurred
 	else {
 		return false;
 	}
 
+	// If all goes well, return that no error occurred
 	return true;
 }
 //------------------------------------------------------------
@@ -61,6 +72,7 @@ int OpenScript(Animation *Anim, const char *sScriptFile)
 //------------------------------------------------------------
 int FreeScript(Animation *Anim)
 {
+	// Unload animation and return if it was successful
 	return Anim->unload();
 }
 //------------------------------------------------------------
@@ -71,6 +83,7 @@ int FreeScript(Animation *Anim)
 //------------------------------------------------------------
 int PlayForwardScript(Animation *Anim)
 {
+	// Play animation forward and return if it was successful
 	return Anim->playForward();
 }
 //------------------------------------------------------------
@@ -81,6 +94,7 @@ int PlayForwardScript(Animation *Anim)
 //------------------------------------------------------------
 int PlayBackwardScript(Animation *Anim)
 {
+	// Play animation backward and return if it was successful
 	return Anim->playBackward();
 }
 //------------------------------------------------------------
--- GenerateurAnimation_GPA665/Laboratoire2.h
+++ GenerateurAnimation_GPA665/Laboratoire2.h
@@ -10,17 +10,15 @@ enum ExecuteMode
 	emPlayBackward
 };
 
-//------------------------------------------------------------
+// Redirection function, excecute on of the other functions depending on ExecMode
 void Execute(ExecuteMode ExecMode, const char *sScriptFile);
 
-//------------------------------------------------------------
+// Functions to load and unload the animation
 int OpenScript(Animation *Anim, const char *sScriptFile);
 int FreeScript(Animation *Anim);
 
-//------------------------------------------------------------
+// Functions to play the animation forward and backward
 int PlayForwardScript(Animation *Anim);
 int PlayBackwardScript(Animation *Anim);
-
-
 
 #endif /* _LABORATOIRE2_H_ */
